a5fc737e330377e38136a7a7c1692fc560ae2182
Kim Min Sung <aidennnn97@gmail.com>
2022-10-10 18:26:00 +0900

    상품등록페이지

--- routes/user/register.js
+++ routes/user/register.js
@@ -6,4 +6,57 @@ router.get('/', function(req, res, next) {
   res.render('register', { title: 'home' });
 });
 
+async function insertProductInfo(prdtName, prdtContent, prdtCnt, prdtPrice, prdtDate) {
+
+  let connection = await oracledb.getConnection(ORACLE_CONFIG);
+
+  let binds = {};
+  let options = {
+      outFormat: oracledb.OUT_FORMAT_OBJECT   // query result format
+    };
+  var sql = "INSERT INTO product(member_id, member_pwd, member_name, member_addr1, member_addr2) VALUES('" + prdtName + "','" + prdtContent + "','" + prdtCnt + "','" + prdtPrice + "','" + prdtDate + "')";
+
+  let result = await connection.execute(sql, binds, options);
+
+  //console.log(result.rows);
+  
+  await connection.close();
+  
+  return result.rows;
+}
+
+async function insertImgInfo(prdtImg1, prdtImg2, prdtImg3, prdtImg4) {
+
+  let connection = await oracledb.getConnection(ORACLE_CONFIG);
+
+  let binds = {};
+  let options = {
+      outFormat: oracledb.OUT_FORMAT_OBJECT   // query result format
+    };
+  var sql = "INSERT INTO product(member_id, member_pwd, member_name, member_addr1, member_addr2) VALUES('" + prdtImg1 + "','" + prdtImg2 + "','" + prdtImg3 + "','" + prdtImg4 + "')";
+
+  let result = await connection.execute(sql, binds, options);
+
+  //console.log(result.rows);
+  
+  await connection.close();
+  
+  return result.rows;
+}
+
+
+
+router.post('/', async function(req, res){
+  var prdtName = req.body.prdtName;
+  var prdtContent = req.body.prdtContent;
+  var prdtCnt = req.body.prdtCnt;
+  var prdtPrice = req.body.prdtPrice;
+  var prdtDate = req.body.prdtDate;
+  var prdtImgs = req.body.prdtImgs;
+  // console.log(prdtName, prdtContent, prdtCnt, prdtPrice, prdtDate, prdtImgs);
+  // await insertProductInfo(prdtName, prdtContent, prdtCnt, prdtPrice, prdtDate);
+  // await insertImgInfo(prdtImg1, prdtImg2, prdtImg3, prdtImg4);
+  res.send("<script>alert('상품등록 성공.'); location.href='/home'</script>");
+});
+
 module.exports = router;
