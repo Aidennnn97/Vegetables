9318e457b19edf4c464268c45b2aeea5fc7bae58
Aidennnn97 <aidennnn97@gmail.com>
2023-01-31 20:39:27 +0900

    ReadMe

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-# Vegetables
- Vegetables Market
+# 야채나라
 
-## 농산물거래사이트
+농산물 거래 웹 사이트
+
+## 개발환경
+ - NodeJS, Ejs, BootStrap
+ - KaKaoPay API, Daum Address API, IamPort API
+ - Express, OracleDB
+
+## 기능
+ - 로그인 / 회원가입
+ - 상품 등록, 삭제
+ - 장바구니
+ - 카카오페이 결제
+ - 아임포트 주문 요청
+
+| ![그림1](/public/images/erd.png) |
+| :---------------------------------: |
+| _ERD_ |
+| ![그림1](/public/images/main.png) |
+| :---------------------------------: |
+| _메인화면_ |
+| ![그림1](/public/images/register.png) |
+| :---------------------------------: |
+| _상품등록화면_ |
+| ![그림1](/public/images/cart.png) |
+| :---------------------------------: |
+| _장바구니_ |
+| ![그림1](/public/images/payment.png) |
+| :---------------------------------: |
+| _결제화면_ |
+| ![그림1](/public/images/address.png) |
+| :---------------------------------: |
+| _다음 주소_ |
+| ![그림1](/public/images/kakaopay.png) |
+| :---------------------------------: |
+| _카카오페이_ |
+| ![그림1](/public/images/iamport.png) |
+| :---------------------------------: |
+| _아임포트 관리자 페이지_ |
--- routes/login.js
+++ routes/login.js
@@ -1,126 +1,122 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var oracledb = require('oracledb');
-const {
-    ORACLE_CONFIG
-} = require("../config/db");
-if (process.platform === 'darwin') {
-    try {
-      oracledb.initOracleClient({libDir: process.env.HOME + '/Downloads/instantclient_19_8'});
-    } catch (err) {
-      console.error('Whoops!');
-      console.error(err);
-      process.exit(1);
-    }
+var oracledb = require("oracledb");
+const { ORACLE_CONFIG } = require("../config/db");
+if (process.platform === "darwin") {
+  try {
+    oracledb.initOracleClient({
+      libDir: process.env.HOME + "/Downloads/instantclient_19_8",
+    });
+  } catch (err) {
+    console.error("Whoops!");
+    console.error(err);
+    process.exit(1);
   }
+}
 
 //로그인 페이지로 이동
-router.get('/', (req, res) => {
-  let route = req.app.get('views') + '/login';
+router.get("/", (req, res) => {
+  let route = req.app.get("views") + "/login";
   res.render(route, { layout: false });
-})
+});
 
 //로그인
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const loginId = req.body.userId;
   const loginPwd = req.body.userPwd;
-  
+
   //아이디, 비밀번호 DB 조회
   result = await selectUserDatabase(loginId, loginPwd);
-  
-  try { // result == undefined 이면 조회되는 id, pwd X
-    
-    if(result == undefined){
 
-      res.send('<script>alert("아이디 또는 비밀번호를 잘못 입력했습니다."); location.href = document.referrer;</script>');
+  try {
+    // result == undefined 이면 조회되는 id, pwd X
 
-    } else{ // 조회되면
+    if (result == undefined) {
+      res.send(
+        '<script>alert("아이디 또는 비밀번호를 잘못 입력했습니다."); location.href = document.referrer;</script>'
+      );
+    } else {
+      // 조회되면
 
       const userName = result.MEMBER_NAME;
       const userNo = result.MEMBER_NO;
       const userAddr1 = result.MEMBER_ADDR1;
       const userAddr2 = result.MEMBER_ADDR2;
       // 일반회원 ,관리자회원 판단
-      if(result.MEMBER_AUTH == '관리자'){
-
-        if(req.session.user){ //session 있으면
-          res.redirect('/admin/main');
-
-        } else{ //session 없으면 생성  
-
-          req.session.user = {
-            sessionId: loginId,
-            sessionNo: userNo,
-            sessionName: userName,
-            sessionAddr1: userAddr1,
-            sessionAddr2: userAddr2
-          };
-          res.redirect('/admin/main');
-
-        }
-
-      } else if(result.MEMBER_AUTH == '일반회원'){
-
-        if(req.session.user){ //session 있으면
-          
-          res.redirect('/user/home');
-        
-        } else{ //session 없으면
+      if (result.MEMBER_AUTH == "관리자") {
+        if (req.session.user) {
+          //session 있으면
+          res.redirect("/admin/main");
+        } else {
+          //session 없으면 생성
 
           req.session.user = {
             sessionId: loginId,
             sessionNo: userNo,
             sessionName: userName,
             sessionAddr1: userAddr1,
-            sessionAddr2: userAddr2
+            sessionAddr2: userAddr2,
           };
-          res.redirect('/user/home');
-
+          res.redirect("/admin/main");
         }
+      } else if (result.MEMBER_AUTH == "일반회원") {
+        if (req.session.user) {
+          //session 있으면
 
-      } 
+          res.redirect("/user/home");
+        } else {
+          //session 없으면
+
+          req.session.user = {
+            sessionId: loginId,
+            sessionNo: userNo,
+            sessionName: userName,
+            sessionAddr1: userAddr1,
+            sessionAddr2: userAddr2,
+          };
+          res.redirect("/user/home");
+        }
+      }
     }
   } catch (error) {
     console.log(error.messages);
   }
-
 });
 
 //로그아웃
-router.get('/logout', async (req, res) => {
+router.get("/logout", async (req, res) => {
   if (req.session.user) {
-      req.session.destroy(function (err) {
-          if (err) throw err;
-          res.send("<script>alert('로그아웃 되었습니다.'); location.href='/'</script>");
-      });
+    req.session.destroy(function (err) {
+      if (err) throw err;
+      res.send(
+        "<script>alert('로그아웃 되었습니다.'); location.href='/'</script>"
+      );
+    });
   } else {
-      res.redirect('/');
+    res.redirect("/");
   }
 });
 
-
 //select
 async function selectUserDatabase(loginId, loginPwd) {
-
   try {
     let connection = await oracledb.getConnection(ORACLE_CONFIG);
-    
-    let sql = "SELECT * FROM member WHERE member_id = :id AND member_pwd = :pwd";
+
+    let sql =
+      "SELECT * FROM member WHERE member_id = :id AND member_pwd = :pwd";
 
     let param = [loginId, loginPwd]; //조건 값
     let options = {
-      outFormat: oracledb.OUT_FORMAT_OBJECT   // query result format
+      outFormat: oracledb.OUT_FORMAT_OBJECT, // query result format
     };
 
     let result = await connection.execute(sql, param, options);
 
     await connection.close();
-    
-    return result.rows[0];
 
+    return result.rows[0];
   } catch (error) {
     console.log(error.messages);
   }
-    
 }
 module.exports = router;
